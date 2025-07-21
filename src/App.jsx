1759e958db2d444e05f4036d152ff6ce43405f47
Akshay-hub-007 <akshaykalangi9@gmail.com>
2025-07-21 10:22:06 +0530

    firt commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <label className='mystyle'>KL University</label>
+    </>
+  )
+}
+
+export default App
